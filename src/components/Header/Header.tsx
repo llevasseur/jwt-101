c759e7f2f13921addea0f25a348423d346252536
Leevon Levasseur <leevonlevasseur@gmail.com>
2025-03-18 15:33:05 -0700

    adds auth context to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,36 @@
 import { Link } from "react-router-dom";
 import "./Header.scss";
+import { useAuth } from "../../context/AuthContext";
 
 const Header = () => {
+  const { user, logout } = useAuth();
+
   return (
     <div className="header-wrapper">
       <header className="header">
         <h2 className="header__title">React Client</h2>
+
         <div className="header__nav">
           <nav className="nav">
             <Link to="/" className="nav__link">
               Home
             </Link>
-            <Link to="/" className="nav__link">
-              Profile
-            </Link>
-            <button className="nav__link">Logout</button>
+            {user ? (
+              <>
+                <Link to="/" className="nav__link">
+                  Profile
+                </Link>
+                <button className="nav__link" onClick={logout}>
+                  Logout
+                </button>
+              </>
+            ) : (
+              <>
+                <Link className="nav__link" to="/login">
+                  Login
+                </Link>
+              </>
+            )}
           </nav>
         </div>
       </header>
